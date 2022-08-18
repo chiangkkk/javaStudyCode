0e0ecea8632c149fddb231c18f63a848d5207118
chiangkai <chiangkk@163.com>
2022-08-18 16:00:47 +0800

    system.getProperties

--- base/src/test/java/org/chiangkai/io/MyIOTest.java
+++ base/src/test/java/org/chiangkai/io/MyIOTest.java
@@ -1,15 +1,19 @@
 package org.chiangkai.io;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class MyIOTest {
     @Test
     public void testWrite(){
         MyIO myIO = new MyIO();
         myIO.transStream();
 
+    }
+    @Test
+    public void t1(){
+        log.info("{}",System.getProperties());
     }
 
 }
--- daily/src/main/java/org/chiangkai/year2022/aug/readme.md
+++ daily/src/main/java/org/chiangkai/year2022/aug/readme.md
@@ -3,3 +3,9 @@
 
 # Day16
 记录一个stream分类的demo
+
+# Day17 
+Q: SpringBoot 的装配？
+A: 通过EnableAutoConfiguration注解，加载
+metainfo/spring.factory下的xxxAutoConfiguation
+当满足Condition时会自动加载
